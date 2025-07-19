81dbd4b4037940570955bbc1428b6891da5bcb19
Ajay Rathore <ajayxrathore@gmail.com>
2025-07-19 20:17:56 +0530

    refactor database connection function for improved readability

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-const connectToDatabase = async() =>{
-    try {
-       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
-       console.log(connectionInstance.connection.host);
-       console.log("Connected to database successfully");
-       
-       
-    } catch (error) {
-        console.log("Error while connecting to database");
-        process.exit(1)
-    }
-}
-export default connectToDatabase
+const connectToDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
+    // console.log(connection.connection.host);
+    console.log("Connected to database successfully");
+  } catch (error) {
+    console.error("Error while connecting to database");
+    process.exit(1);
+  }
+};
+export default connectToDatabase;
