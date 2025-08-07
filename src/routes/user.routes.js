d9a50381215b46c330be285ee0e4c4cf4c3b36ab
Ajay Rathore <ajayxrathore@gmail.com>
2025-08-07 12:02:39 +0530

    refactor user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,20 +1,59 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+
+import {
+  currentUser,
+  getUserChannel,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAvatar,
+  updateCoverImage,
+  updatePassword,
+  updateUser,
+} from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
+
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {name: "avatar", maxCount: 1},
-        {name: "coverImage", maxCount: 1}
-    ]),
-    registerUser
+router
+  .route("/register")
+  .post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
 );
-router.route("/login").post(loginUser)
 
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("update-password").post(verifyJWT, updatePassword);
+
+router.route("/current-user").get(verifyJWT, currentUser);
+
+router.route("/update-user").patch(verifyJWT, updateUser);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannel);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
+
 export default router;
